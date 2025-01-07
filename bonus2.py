b153348c6305695dbfdd894d238b622589f44510
glaudistonneto <glaudistonson@gmail.com>
2025-01-07 01:54:29 -0300

    finishing the todo list with some bonuses

---
+++ bonus2.py
@@ -0,0 +1,7 @@
+password = input('Enter Password: ')
+
+while password != '123':
+    password = input('Enter Password: ')
+
+print('Password was corrct!')
+
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 prompt = 'Enter a todo: '
-todo1 = input(prompt)
-todo2 = input(prompt)
-todo3 = input(prompt)
-todos = [todo1+', ', todo2+', ', todo3+', ']
-print(todos)
+
+todos = []
+
+while True:
+    todo = input(prompt)
+    todos.append(todo.title())
+    print(todos)
