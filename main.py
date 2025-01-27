29d41b327857f7875aa699abbb8d4299f7aaab1c
glaudistonneto <glaudistonson@gmail.com>
2025-01-27 20:43:04 -0300

    More todos on the Python code

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-prompt = 'Type add, show or display, or exit: '
+prompt = 'Type add, show or display, edit or exit: '
 
 todos = []
 
 while True:
     user_action = input(prompt)
+    user_action = user_action.strip()
+
     match user_action:
         case 'add':
             todo = input('Enter a todo: ')
@@ -12,7 +14,14 @@ while True:
             for item in todos:
                 item = item.capitalize()
                 print(item)
+        case 'edit':
+            number = int(input('Enter the number of todo to edit: '))
+            number = number -1
+            new_todo = input('Enter new todo: ')
+            todos[number] = new_todo
         case 'exit':
             break
         case whatever_it_is:
             print('Hey! You entered an unknown command.')
+
+print('Bye!')
