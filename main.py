e4a2ab345442fdf24cf90c2eeb59ab54200b044f
glaudistonneto <glaudistonson@gmail.com>
2025-01-31 15:01:27 -0300

    separed in folders, added save in text file

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-prompt = 'Type add, show or display, edit, complete, or exit: '
-
-todos = []
-
 while True:
-    user_action = input(prompt)
+    user_action = input('Type add, show or display, edit, complete, or exit: ')
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
-            todo = input('Enter a todo: ')
+            todo = input('Enter a todo: ') + '\n'
+
+            file = open('files/todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo.capitalize())
+
+            file = open('files/todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
         case 'show' | 'display':
+            file = open('files/todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             for index, item in enumerate(todos):
                 item = item.capitalize()
                 row = f"{index + 1}.{item}"
+                print(row)
+        case 'show' | 'display':
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+            file.close()
+
+            for index, item in enumerate(todos):
+                item = item.strip('\n').capitalize()
+                row = f"{index + 1}.) {item}"
                 print(row)
         case 'edit':
             number = int(input('Enter the number of todo to edit: '))
