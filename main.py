09c90a989389f0db6efa625cdb8ed954c13d2bc6
glaudistonneto <glaudistonson@gmail.com>
2025-02-05 17:35:50 -0300

    improving user interface on 'show'

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 while True:
+    # Get user input and strip space chars from it
     user_action = input('Type add, show or display, edit, complete, or exit: ')
     user_action = user_action.strip()
 
@@ -6,44 +7,50 @@ while True:
         case 'add':
             todo = input('Enter a todo: ') + '\n'
 
-            file = open('files/todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
 
             todos.append(todo.capitalize())
 
-            file = open('files/todos.txt', 'w')
-            file.writelines(todos)
-            file.close()
-        case 'show' | 'display':
-            file = open('files/todos.txt', 'r')
-            todos = file.readlines()
-            file.close()
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
 
-            for index, item in enumerate(todos):
-                item = item.capitalize()
-                row = f"{index + 1}.{item}"
-                print(row)
         case 'show' | 'display':
             with open('files/todos.txt', 'r') as file:
                 todos = file.readlines()
-            file.close()
 
-            for index, item in enumerate(todos):
+            # new_todos = []
+            # for item in todos:
+            #     new_item = item.strip('\n')
+            #     new_todos.append(new_item)
+
+            # new_todos = [item.strip('\n') for item in todos]
+
+            for index, item in enumerate(todos): # new_todos for the previous two
                 item = item.strip('\n').capitalize()
-                row = f"{index + 1}.) {item}"
+                row = f"{index + 1}. {item}"
                 print(row)
+
         case 'edit':
             number = int(input('Enter the number of todo to edit: '))
-            number = number -1
+            number = number - 1
             new_todo = input('Enter new todo: ')
-            todos[number] = new_todo
+            todos[number] = new_todo + '\n'
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case 'complete':
             number = int(input('Enter the number of todo to complete: '))
-            todos.pop(number -1)
+            todos.pop(number - 1)
+
+            with open('files/todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case 'exit':
             break
-        case whatever_it_is:
+
+        case _:
             print('Hey! You entered an unknown command.')
 
 print('Bye!')
