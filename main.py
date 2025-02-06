921957454a6879e901c54a571808196205456e1f
glaudistonneto <glaudistonson@gmail.com>
2025-02-06 16:45:45 -0300

    completed editing, and 'complete' as improving user interface

--- main.py
+++ main.py
@@ -34,6 +34,10 @@ while True:
         case 'edit':
             number = int(input('Enter the number of todo to edit: '))
             number = number - 1
+
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+
             new_todo = input('Enter new todo: ')
             todos[number] = new_todo + '\n'
 
@@ -42,10 +46,20 @@ while True:
 
         case 'complete':
             number = int(input('Enter the number of todo to complete: '))
-            todos.pop(number - 1)
+
+            with open('files/todos.txt', 'r') as file:
+                todos = file.readlines()
+
+            index = number  - 1
+            todo_to_remove = todos[index].strip('\n')
+
+            todos.pop(index)
 
             with open('files/todos.txt', 'w') as file:
                 file.writelines(todos)
+
+            message = f'Todo {todo_to_remove} was removed from the list.'
+            print(message)
 
         case 'exit':
             break
