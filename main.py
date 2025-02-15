202785a086e34eef905692642d953222a991a7ca
glaudistonneto <glaudistonson@gmail.com>
2025-02-15 20:09:08 -0300

    external functions, parsers bonus, corrected bonus13

--- main.py
+++ main.py
@@ -1,70 +1,51 @@
+# from modules.functions import get_todos, write_todos "not necessary functions. before get_todos and write_todos", but requires __init__.py inside modules directory
+from modules import functions
+
 while True:
-    # Get user input and strip space chars from it
     user_action = input('Type add, show or display, edit, complete, or exit: ')
     user_action = user_action.strip()
 
-    match user_action:
-        case 'add':
-            todo = input('Enter a todo: ') + '\n'
+    if user_action.startswith('add'):
+        todo = user_action[4:]
+        todos = functions.get_todos()
+        todos.append(todo.capitalize() + '\n')
+        functions.write_todos(todos)
 
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
+    elif user_action.startswith('show') or user_action.startswith('display'):
+        todos = functions.get_todos()
+        for index, item in enumerate(todos):
+            item = item.strip('\n').capitalize()
+            print(f"{index + 1}. {item}")
 
-            todos.append(todo.capitalize())
-
-            with open('files/todos.txt', 'w') as file:
-                file.writelines(todos)
-
-        case 'show' | 'display':
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
-
-            # new_todos = []
-            # for item in todos:
-            #     new_item = item.strip('\n')
-            #     new_todos.append(new_item)
-
-            # new_todos = [item.strip('\n') for item in todos]
-
-            for index, item in enumerate(todos): # new_todos for the previous two
-                item = item.strip('\n').capitalize()
-                row = f"{index + 1}. {item}"
-                print(row)
-
-        case 'edit':
-            number = int(input('Enter the number of todo to edit: '))
+    elif user_action.startswith('edit'):
+        try:
+            number = int(user_action[5:])
             number = number - 1
-
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
-
+            todos = functions.get_todos()
             new_todo = input('Enter new todo: ')
             todos[number] = new_todo + '\n'
+            functions.write_todos(todos)
+        except ValueError:
+            print('Expected the number of the todo instead of a name.')
+            continue
 
-            with open('files/todos.txt', 'w') as file:
-                file.writelines(todos)
-
-        case 'complete':
-            number = int(input('Enter the number of todo to complete: '))
-
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
-
-            index = number  - 1
+    elif user_action.startswith('complete'):
+        try:
+            number = int(user_action[9:])
+            todos = functions.get_todos()
+            index = number - 1
             todo_to_remove = todos[index].strip('\n')
-
             todos.pop(index)
+            functions.write_todos(todos)
+            print(f'Todo {todo_to_remove} was removed from the list.')
+        except IndexError:
+            print("There's no item with that number.")
+            continue
 
-            with open('files/todos.txt', 'w') as file:
-                file.writelines(todos)
+    elif user_action.startswith('exit'):
+        break
 
-            message = f'Todo {todo_to_remove} was removed from the list.'
-            print(message)
-
-        case 'exit':
-            break
-
-        case _:
-            print('Hey! You entered an unknown command.')
+    else:
+        print('Hey! You entered an unknown command.')
 
 print('Bye!')
