0c5977940910a9629d1335520e21505eb12bff14
glaudistonneto <glaudistonson@gmail.com>
2025-02-18 17:43:19 -0300

    added bonus16, and corrected bonnus.15 to bonus15

---
+++ bonus/bonus16.py
@@ -0,0 +1,19 @@
+import FreeSimpleGUI as fsg
+
+label1 = fsg.Text("Select files to compress")
+input1 = fsg.Input()
+choose_button1 = fsg.FilesBrowse("Choose")
+
+label2 = fsg.Text("Select destination folder")
+input2 = fsg.Input()
+choose_button2 = fsg.FolderBrowse("Choose")
+
+compress_button = fsg.Button("Compress")
+
+window = fsg.Window("File Compressor",
+                    layout=[[label1, input1,  choose_button1],
+                            [label2, input2, choose_button2],
+                            [compress_button]])
+
+window.read()
+window.close()
